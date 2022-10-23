da8af45759904b570ecb77c4821eb1c7d0965f13
Aniruddha1261 <thakreaniruddha1@gmail.com>
2022-10-24 02:01:49 +0530

    Backtracking code added

--- Notes/Code/5_Recursion/Recursion_05/BackTracking/src/Maze.java
+++ Notes/Code/5_Recursion/Recursion_05/BackTracking/src/Maze.java
@@ -1,11 +1,19 @@
 import java.util.ArrayList;
 
 public class Maze {
+
     public static void main(String[] args) {
         System.out.println(count(3,3));
         path("", 3, 3);
         System.out.println(pathRet("", 3, 3));
         System.out.println(pathRetDiagonal("" , 3, 3));
+
+        boolean[][] board = {
+                {true, true, true},
+                {true, false, true},
+                {true, true, true}
+        };
+        pathRestrictions("", board, 0, 0);
     }
 
     // print the number of paths to reach a point in a maze.
@@ -15,7 +23,7 @@ public class Maze {
         }
 
         int left = count(r - 1, c);
-        int right = count(r, c -1 );
+        int right = count(r, c - 1 );
 
 
         return left + right;
@@ -75,5 +83,21 @@ public class Maze {
         }
 
         return list;
+    }
+
+    static void pathRestrictions(String p, boolean[][] maze, int r, int c){
+        if (r == maze.length - 1 && c == maze[0].length - 1){
+            System.out.println(p);
+            return;
+        }
+        if (!maze[r][c]){
+            return;
+        }
+        if (r < maze.length - 1){
+            pathRestrictions(p + 'D', maze, r + 1, c);
+        }
+        if (c < maze[0].length - 1){
+            pathRestrictions(p + 'R', maze, r, c + 1);
+        }
     }
 }
