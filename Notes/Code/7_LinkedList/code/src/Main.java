523d421ea4dff68102555152827793cb08ed99a0
Aniruddha1261 <thakreaniruddha1@gmail.com>
2023-03-12 03:00:28 +0530

    Complete notes of singly linked list

--- Notes/Code/7_LinkedList/code/src/Main.java
+++ Notes/Code/7_LinkedList/code/src/Main.java
@@ -7,6 +7,13 @@ public class Main {
         list.insertFirst(8);
         list.insertFirst(17);
         list.insertLast(99);
+        list.insert(100, 2);
+        list.display();
+        System.out.println(list.deleteFirst() + " is deleted.");
+        list.display();
+        System.out.println(list.deleteLast() + " is deleted.");
+        list.display();
+        System.out.println(list.delete(2) + " is deleted.");
         list.display();
     }
 }
