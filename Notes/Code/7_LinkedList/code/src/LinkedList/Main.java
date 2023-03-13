4b40ba70a3180bcabce3b579e65e4ab2cbf5db6b
Aniruddha1261 <thakreaniruddha1@gmail.com>
2023-03-14 02:06:23 +0530

    Circular Linked List implementation notes

--- Notes/Code/7_LinkedList/code/src/LinkedList/Main.java
+++ Notes/Code/7_LinkedList/code/src/LinkedList/Main.java
@@ -1,7 +1,9 @@
+package LinkedList;
+
 public class Main {
     public static void main(String[] args) {
         // Singly Linked List
-//        LL list = new LL();
+//        LinkedList.LL list = new LinkedList.LL();
 //        list.insertFirst(3);
 //        list.insertFirst(2);
 //        list.insertFirst(8);
@@ -17,13 +19,25 @@ public class Main {
 //        list.display();
 
         // Doubly Linked List
-        DLL list2 = new DLL();
-        list2.insertFirst(5);
-        list2.insertFirst(8);
-        list2.insertFirst(7);
-        list2.insertFirst(3);
-        list2.insertLast(10);
-        list2.insertAfter(10, 55);
-        list2.display();
+//        DLL list2 = new DLL();
+//        list2.insertFirst(5);
+//        list2.insertFirst(8);
+//        list2.insertFirst(7);
+//        list2.insertFirst(3);
+//        list2.insertLast(10);
+//        list2.insertAfter(8, 55);
+//        list2.display();
+
+
+        // Circular Linked List
+        CLL list3 = new CLL();
+        list3.insert(1);
+        list3.insert(2);
+        list3.insert(3);
+        list3.insert(4);
+        list3.display();
+        list3.delete(3);
+        list3.display();
+
     }
 }
