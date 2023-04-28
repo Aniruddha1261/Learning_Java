9629ed22b83a3cbdfb01b8d15b56a4f492afe5fd
Aniruddha1261 <thakreaniruddha1@gmail.com>
2023-04-28 21:28:04 +0530

    Questions on heaps

---
+++ Notes/Code/09_RemainingDataStructures/code/src/Heaps/Q2_ConnectN_Ropes.java
@@ -0,0 +1,8 @@
+package Heaps;
+// Given are N ropes of different lengths, the task is to connect these ropes into one rope with
+// minimum cost, such that the cost to connect two ropes is equal to the sum of their lengths.
+public class Q2_ConnectN_Ropes {
+    public static void main(String[] args) {
+        int[] ropes = {4, 3, 2, 6};
+    }
+}
